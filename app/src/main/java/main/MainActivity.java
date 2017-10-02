2f6b77eb4e6ddb77eda9f074217c46c1e16f60e2
pellesam <pellesam.info@gmail.com>
2017-10-02 09:19:43 +0200

    First commit

---
+++ app/src/main/java/main/MainActivity.java
@@ -0,0 +1,25 @@
+package main;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.info.pellesam.securityproject.R;
+
+/**
+ * Created by pellesam on 02/10/2017.
+ */
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_view);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
