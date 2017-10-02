692c4f051c2efe919d57af598ab9d3942d511be7
pellesam <pellesam.info@gmail.com>
2017-10-02 11:17:31 +0200

    Add toast

--- app/src/main/java/com/info/pellesam/securityproject/Custom/CustomViewAdapterCategories.java
+++ app/src/main/java/com/info/pellesam/securityproject/Custom/CustomViewAdapterCategories.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.info.pellesam.securityproject.Entity.Category;
 import com.info.pellesam.securityproject.R;
@@ -16,12 +17,12 @@ import java.util.ArrayList;
  * Created by pellesam on 02/10/2017.
  */
 
-public class CustomViewAdapterCategories extends ArrayAdapter<Category> {
+public class CustomViewAdapterCategories extends ArrayAdapter<Category> implements View.OnClickListener{
 
     private final Context context;
     private final ArrayList<Category> categories;
 
-    public CustomViewAdapterCategories(Context context, ArrayList<Category> categories) {
+    public CustomViewAdapterCategories(Context context, ArrayList<Category> categories){
         super(context, R.layout.list_category_view, categories);
         this.context = context;
         this.categories = categories;
@@ -35,7 +36,17 @@ public class CustomViewAdapterCategories extends ArrayAdapter<Category> {
 
         categoryTitle.setText(categories.get(position).getTitle());
 
+        categoryTitle.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(context, "Merci d'avoir cliquer", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
         return rowView;
     }
 
+    public void onClick(View v) {
+
+    }
 }
